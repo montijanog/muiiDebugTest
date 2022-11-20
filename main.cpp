f86ee46e4d0bc0a7c25ebf60f35a80fb6f431bec
Miguel <j.montijano@opendeusto.es>
2022-11-20 19:16:10 +0100

    nuevos cambios

--- main.cpp
+++ main.cpp
@@ -59,10 +59,10 @@ float calcularRMS(int16_t *datos, int longitud)
     float rms=0;
     float datoV;
     for (int i=0;i<longitud;i++){
-        datoV=datos[i]/32768*3.3;
+        datoV=datos[i];
         rms+=datoV*datoV;
     }
-    rms=sqrt(rms/longitud);
+    rms=sqrt(rms/longitud/32768.0*3.3/32768.0*3.3);
     return rms;
 }
  
